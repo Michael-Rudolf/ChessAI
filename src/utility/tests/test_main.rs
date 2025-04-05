ebac8cafe928f56824c274cfbab0cbfe4027a8e1
MichaelRudol <98760082+MichaelRudol@users.noreply.github.com>
2025-04-05 18:23:53 +0200

    I mean something works

--- src/utility/tests/test_main.rs
+++ src/utility/tests/test_main.rs
@@ -1,7 +1,5 @@
-use rand::random_range;
 use crate::utility::layer::Layer;
 use crate::utility::network::Network;
-use crate::utility::neuronal_layer::neuronal_layer;
 use crate::utility::neuronal_layer::neuronal_layer::{ActivationFunction, NeuronalLayer};
 use crate::utility::training_data::TrainingData;
 
@@ -23,18 +21,20 @@ pub(crate) fn test() {
     println!("0^0: {:?}", network.get_outputs());
 
 
-    /*for i in 0..2 {
+    for i in 0..2 {
         for j in 0..2 {
-            let shouldReturnTrue = (i == 1 && j == 0) || (i == 0 && j == 1); //|| (i == 0 && j == 1);// || (i == 0 && j == 1);// j == 0);// || (i == 0 && j == 1);//i == 1;//j == 1 && i == 0;//(i == 1 && j == 0) || (i == 0 && j == 1);
-            println!("shouldReturnTrue: {}, i: {}, j: {}", shouldReturnTrue, i, j);
-            let something = if shouldReturnTrue { 1f32 } else { 0.1f32 };
+            let should_return_true = (i == 1 && j == 0) || (i == 0 && j == 1); //|| (i == 0 && j == 1);// || (i == 0 && j == 1);// j == 0);// || (i == 0 && j == 1);//i == 1;//j == 1 && i == 0;//(i == 1 && j == 0) || (i == 0 && j == 1);
+            println!("should_return_true: {}, i: {}, j: {}", should_return_true, i, j);
+            let something = if should_return_true { 1f32 } else { 0.1f32 };
             let training_data = TrainingData::new(vec![i as f32, j as f32], vec![something]);
             network.add_training_data(training_data);
         }
-    }*/
+    }
     network
-        .add_training_data(TrainingData::new(vec![1.0, 0.0], vec![1.0]))
-        .add_training_data(TrainingData::new(vec![0.0, 1.0], vec![1.0]));
+        .add_training_data(TrainingData::new(vec![1.0, 1.0], vec![0.0]));
+
+    network
+        .add_training_data(TrainingData::new(vec![1.0, 0.0], vec![1.0]));
 
 
     /**/for _ in 0..20000 {
